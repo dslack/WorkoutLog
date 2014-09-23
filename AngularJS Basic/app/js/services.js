6e6983727b60460b31ce963e4e5090071eb861fd
Dan Slack <dslack@langui.com>
2014-09-23 09:01:53 -0500

    added storage service for initial app state

--- AngularJS Basic/app/js/services.js
+++ AngularJS Basic/app/js/services.js
@@ -1,1 +1,17 @@
-angular.module('services',[]);
+angular.module('services',[])
+    .factory('Storage', function(){
+        var _storage = window.localStorage;
+        return {
+            get: function(key){
+                var _item = _storage.getItem(key);
+                if (!_item) {
+                    return [];
+                }
+                return JSON.parse(_item);
+            },
+            set: function(key, value){
+                _storage.removeItem(key);
+                _storage.setItem(key, JSON.stringify(value));
+            }
+        };
+    });
