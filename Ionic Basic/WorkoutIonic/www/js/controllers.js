ef25b1453766363714d40ac4ed3895c01285be51
Dan Slack <dan@terrazzotechnologies.com>
2014-09-23 10:16:46 -0500

    Updated Ionic Base to have the code we *should* have at the end of the Angular Demo

--- Ionic Basic/WorkoutIonic/www/js/controllers.js
+++ Ionic Basic/WorkoutIonic/www/js/controllers.js
@@ -1,5 +1,22 @@
 angular.module('controllers', ['services'])
-.controller('HistoryController', function($scope, $ionicModal, Storage){})
-.controller('WorkoutController', function($scope, WorkoutData) {
-  $scope.workoutData = WorkoutData;
+.controller('HistoryController', function($scope, $ionicModal, Storage){
+    $scope.data = {
+        history: Storage.get("workout")
+    };
+    $scope.clearWorkouts = function(){
+        Storage.set("workout", []);
+        $scope.data.history = Storage.get("workout");    
+    }
+})
+.controller('WorkoutController', function($scope, WorkoutData, Storage) {
+    $scope.data = {	
+        workouts: WorkoutData.getWorkouts()
+    };
+
+    $scope.addWorkout = function(){
+        //We have the workout available...
+        WorkoutData.addWorkout($scope.data.workoutType);
+        $scope.data.workout = null;
+        $scope.data.history = Storage.get("workout");
+    };
 });
--- Ionic Basic/WorkoutIonic/www/js/services.js
+++ Ionic Basic/WorkoutIonic/www/js/services.js
@@ -1,7 +1,4 @@
 angular.module('services', [])
-.factory('WorkoutData', function(){
-  return window.WORKOUT;
-})
 .factory('Storage', function(){
     var _storage = window.localStorage;
     return {
@@ -15,6 +12,28 @@ angular.module('services', [])
         set: function(key, value){
             _storage.removeItem(key);
             _storage.setItem(key, JSON.stringify(value));
+        }
+    };
+})
+.factory('WorkoutData', function(Storage){
+    var workoutData = window.WORKOUT;
+    var workouts = workoutData.workouts;
+
+    return {
+        getWorkouts: function(){
+            return workouts;
+        },
+        addWorkout: function(workoutName) {
+            var toAddWorkout = _.find(workouts, function(workout){
+                return (workoutName === workout.type);
+            });
+
+            toAddWorkout.workoutDate = new Date();
+
+            //add to local storage.
+            var storedWorkouts = Storage.get("workout");
+            storedWorkouts.push(toAddWorkout);
+            Storage.set("workout", storedWorkouts);
         }
     };
 });
